f7476b816bd910d34baa87b218fc8ecbaa7d2491
chudasama-rudra <rudracudasma25@gmail.com>
2026-03-13 21:34:41 +0530

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,13 +1,26 @@
 <?php
-$server = "localhost";
-$database = "outfit_aura";
-$username = "root";
-$password = "";
+//$server = "localhost";
+//$database = "outfit_aura";
+//$username = "root";
+//$password = "";
 
-$cost = mysqli_connect("localhost", $username, $password, $database);
+//$cost = mysqli_connect("localhost", $username, $password, $database);
 // if ($cost->connect_error) {
 //     die("Connection failed: " . $conn->connect_error);
 //   }
 //   echo "Connected successfully";
 
+?>
+<?php
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = (int)getenv('MYSQLPORT');
+
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
